913fb2207d883ef88b497c4cb23da216d91a7285
Ian Chen <czlianlap@gmail.com>
2020-08-23 19:36:04 -0500

    first commit

---
+++ js/lab10-walkthrough05.js
@@ -0,0 +1,15 @@
+/* put your DOM code here */
+
+$(function () {
+// create a new DOM element
+    var img = $('<img src="images/art/thumbs/13030.jpg" >');
+// and now add the new element after the panel
+    var panel = $('.panel');
+//    panel.after(img);
+//    panel.before(img);
+//    panel.append(img);
+//    panel.prepend(img);
+//    img.appendTo(panel);
+//    img.prependTo(panel);
+//    img.insertBefore(panel);
+});
